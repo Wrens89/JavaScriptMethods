fa506067ed18d16109cd1a41327f74549ba18ba3
Wrens89 <r_89_9@hotmail.com>
2021-06-30 20:18:40 +0200

    huiswerk af

--- string-methods.js
+++ string-methods.js
@@ -13,6 +13,7 @@ console.log(word.length);
 const fruit = "Banaan";
 const vegetable = "Aardappel";
 
+console.log(fruit.length > vegetable.length);
 
 
 /* Opdracht 2 */
@@ -20,7 +21,13 @@ const vegetable = "Aardappel";
 //  Tip: bedenk 1 manier die voor beide woorden zou werken.
 // ---- Verwachte uitkomst voor fruit: "n"
 // ---- Verwachte uitkomst voor vegetable: "l"
+const lastIndexFruit = fruit.length - 1;
+const lastIndexVegetable = vegetable.length - 1;
 
+const lastFruit = fruit.substring(lastIndexFruit);
+const lastVegetable = vegetable.substring(lastIndexVegetable);
+
+console.log(lastFruit, lastVegetable);
 
 
 /* Opdracht 3 */
@@ -28,21 +35,24 @@ const vegetable = "Aardappel";
 // ---- Verwachte uitkomst: "De kat krabt de krullen van de trap"
 
 const userInput = "  De kat krabt de krullen van de trap ";
-
-
+const small = userInput.trim();
+console.log(small);
 
 /* Opdracht 4 */
 // Check of het woord "Fantastisch" in onderstaande zin voorkomt en log dit in de terminal.
 // ---- Verwachte uitkomst: true
 
 const story = "We vinden de behaalde resultaten werkelijk prachtig. Fantastisch, nietwaar?"
-
+const fantastic = story.includes("Fantastisch");
+console.log(fantastic);
 
 
 /* Opdracht 5 */
 // Knip de vraag van bovenstaande tekst (story) af, zodat alleen het eerste gedeelte overblijft en log dit in de terminal.
 // ---- Verwachte uitkomst: "We vinden de behaalde resultaten werkelijk prachtig."
-
+const short = story.indexOf(".");
+const sentence = story.substring(0, short + 1);
+console.log(sentence);
 
 
 /* Opdracht 6 */
@@ -54,7 +64,9 @@ const story = "We vinden de behaalde resultaten werkelijk prachtig. Fantastisch,
 
 const price = "$35.59";
 
+const euro = price.replace("$", "€").replace(".", ",");
+console.log(euro);
 
 
 
-
+//test
